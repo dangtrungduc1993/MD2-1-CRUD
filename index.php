42a012887ac67d965fe401b9437cc9ed1a6cd13c
dangtrungduc1993 <trungduc.hou@gmail.com>
2022-02-14 14:57:06 +0700

    add page to index

--- index.php
+++ index.php
@@ -1,13 +1,35 @@
 <?php
 //require "vendor/autoload.php";
-require __DIR__."/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
+
 use App\Model\PostModel;
 use App\Controller\PostController;
 
+$postController = new PostController();
 
-//$postModel = new PostModel();
-//var_dump($postModel->getAll());
-$postController = new PostController();$postController->getAllPost();
+$page = $_GET['page'] ?? '';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a href="index.php">Home</a>
+<a href="index.php?page=post-list">Post</a>
+<?php
+switch ($page) {
+    case "post-list":
+        $postController->getAllPost();
+        break;
+    default:
+}
 
 
 ?>
+</body>
+</html>
